8c7976b587918b0afd9d2436722126acaed81a77
Your Name <you@example.com>
2025-10-13 08:21:06 +0000

    byte

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,10 +13,10 @@ long long c;
 float d;
 char e;
 
-printf("Size of a char:%zu bytes\n", sizeof(e));
-printf("Size of an int:%zu bytes\n", sizeof(a));
-printf("Size of a long int: %zu bytes\n", sizeof(b));
-printf("Size of a long long int: %zu bytes\n", sizeof(c));
-printf("Size of a float: %zu bytes\n", sizeof(d));
+printf("Size of a char: %zu byte(s)\n", sizeof(e));
+printf("Size of an int: %zu byte(s)\n", sizeof(a));
+printf("Size of a long int: %zu byte(s)\n", sizeof(b));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(c));
+printf("Size of a float: %zu byte(s)\n", sizeof(d));
 return (0);
 }
