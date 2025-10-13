83dc69e0364018c8fbab0ca61dd7018a55ab1a71
Your Name <you@example.com>
2025-10-13 15:32:44 +0000

    gcc if

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,17 +21,17 @@ n = rand() - RAND_MAX / 2;
 dl = n % 10;
 /* your code goes there */
 
-if (n > 5)
+if (dl > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n,dl);
+printf("Last digit of %d is %d and is greater than 5\n", n, dl);
 }
-else if (n == 0)
+else if ( dl == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n,dl);
+printf("Last digit of %d is %d and is 0\n", n, dl);
 }
-else if (n < 6 && n != 0)
+else if (dl < 6 && dl != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n,dl);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, dl);
 }
 return (0);
 }
