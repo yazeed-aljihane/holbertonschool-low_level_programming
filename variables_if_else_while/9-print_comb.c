9e5180d79e8a556a50d7cfdb566a51e9bd2c7763
Your Name <you@example.com>
2025-10-14 06:00:11 +0000

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+if (i == 9)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
