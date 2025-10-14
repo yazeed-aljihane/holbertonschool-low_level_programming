e73d59c545cceb1c0ae6d8850ad4e16c3b67bbaa
Your Name <you@example.com>
2025-10-14 05:27:43 +0000

    qe

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a == 'q' || a == 'e')
+{
+continue;
+}
+
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
