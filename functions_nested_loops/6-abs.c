9b22945866151cf265e30598fbf5d0bb2297b8d2
Your Name <you@example.com>
2025-10-15 06:43:11 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - return absoulte value
+ * @n: the input number
+ * Return: n * -1 if n < 0 , n if n >= 0.
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}
+}
