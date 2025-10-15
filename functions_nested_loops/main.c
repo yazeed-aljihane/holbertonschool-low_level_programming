02dda84bddaf727bee99c621ca79d5bd53e307dd
Your Name <you@example.com>
2025-10-15 09:38:03 +0000

    last digit

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -9,20 +9,10 @@
 int main(void)
 {
 int r;
-
-r = print_last_digit(98);
-_putchar(r + '0');
-_putchar(r + '0');
-r = print_last_digit(0);
-_putchar(r + '0');
+print_last_digit(98);
+print_last_digit(0);
 r = print_last_digit(-1024);
-_putchar(r + '0');
-r = print_last_digit(INT_MAX);
-_putchar(r + '0');
-r = print_last_digit(INT_MIN);
-_putchar(r + '0');
-r = print_last_digit(1);
-_putchar(r + '0');
+_putchar('0' + r);
 _putchar('\n');
 return (0);
 }
