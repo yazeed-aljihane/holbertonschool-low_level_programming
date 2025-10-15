550ac43f83ac5241a13cc84c295b052ced1eb4d4
Your Name <you@example.com>
2025-10-15 05:40:39 +0000

    alphaibetX10git add .

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print all alphabet 10X times!.
+ */
+void print_alphabet_x10(void)
+{
+char a;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
+
+
+
+
+
