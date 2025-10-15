49fe2e22ed20607f1153abc9e3e5058910250554
Your Name <you@example.com>
2025-10-15 09:18:04 +0000

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,10 @@
  */
 int print_last_digit(int n)
 {
+n = n % 10;
 if (n < 0)
 {
-n = n * -1;
+n = -n;
 }
-n = n % 10;
 return (n);
 }
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <limits.h>
 /**
  * main - check the code
  *
@@ -12,10 +12,16 @@ int r;
 
 r = print_last_digit(98);
 _putchar(r + '0');
+_putchar(r + '0');
 r = print_last_digit(0);
 _putchar(r + '0');
 r = print_last_digit(-1024);
 _putchar(r + '0');
+r = print_last_digit(INT_MAX);
+_putchar(r + '0');
+r = print_last_digit(INT_MIN);
+_putchar(r + '0');
+r = print_last_digit(1);
 _putchar(r + '0');
 _putchar('\n');
 return (0);
