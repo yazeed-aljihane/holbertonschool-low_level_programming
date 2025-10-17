e9c42b2f46f01d01a4f937047131fdd76b93744c
Your Name <you@example.com>
2025-10-17 08:21:40 +0000

    98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_to_98 - print from any natural number to 98
+ * @n: input number
+ */
+void print_to_98(int n)
+{
+int i;
+
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else
+{
+printf("%d, ", i);
+}
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else
+{
+printf("%d, ", i);
+}
+}
+}
+printf("\n");
+}
+
+
+
+
+
+
