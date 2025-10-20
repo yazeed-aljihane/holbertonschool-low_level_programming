d3050f437084a48afc91a313d901c0a41ca18ef9
Your Name <you@example.com>
2025-10-20 15:21:38 +0000

    squer

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * print_square - print_square
+ * @size: the size of square
+ */
+void print_square(int size)
+{
+if (size > 0)
+{
+int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+
+
+}
+else
+{
+_putchar('\n');
+}
+}
