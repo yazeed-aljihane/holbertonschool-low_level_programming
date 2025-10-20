d07e721b0404beab1b00d85cf5b87b2d870907ee
Your Name <you@example.com>
2025-10-20 06:35:25 +0000

    is digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+
+/**
+ * _isdigit - check if the input is digit
+ * @c: the input
+ * Return: 1 if didgit 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
