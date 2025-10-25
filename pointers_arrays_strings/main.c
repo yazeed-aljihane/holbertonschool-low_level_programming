8fc9e1e0baedcac7629c7954b6b8d13ddf68e348
Your Name <you@example.com>
2025-10-25 21:42:25 +0000

    leet

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -9,13 +9,24 @@
 int main(void)
 {
 
-char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-char *p;
+int nb;
 
-p = leet(s);
-printf("%s", p);
-printf("%s", s);
-
+nb = _atoi("98");
+printf("%d\n", nb);
+nb = _atoi("-402");
+printf("%d\n", nb);
+nb = _atoi("          ------++++++-----+++++--98");
+printf("%d\n", nb);
+nb = _atoi("214748364");
+printf("%d\n", nb);
+nb = _atoi("0");
+printf("%d\n", nb);
+nb = _atoi("Suite 402");
+printf("%d\n", nb);
+nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+printf("%d\n", nb);
+nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+printf("%d\n", nb);
 
 return (0);
 }
