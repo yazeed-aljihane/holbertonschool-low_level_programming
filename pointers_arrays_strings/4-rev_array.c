55f71db571a9333d36164f226b32edec8935f467
Your Name <you@example.com>
2025-10-25 15:30:33 +0000

    arry

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -31,28 +31,6 @@ a[n - 1 - i] = tmp;
 
 
 
-/**
- * print_array - prints an array of integers
- * @a: array of integers
- * @n: number of elements
- *
- * Return: nothing
- */
-void print_array(int *a, int n)
-{
-int i;
-
-for (i = 0; i < n; i++)
-{
-if (i != 0)
-printf(", ");
-printf("%d", a[i]);
-}
-printf("\n");
-}
-
-
-
 
 
 
