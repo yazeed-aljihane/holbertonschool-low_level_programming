663076dc0a9c12e6d2e249e700ca0a0064017069
Your Name <you@example.com>
2025-10-25 08:32:10 +0000

    strcat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+
+/**
+ * *_strncat - append 2 str with each other
+ * @dest: str1
+ * @src: str2
+ * @n: number of letter fron str2
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+int k = 0;
+if (src[0] != '\0')
+{
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+
+
+dest[i] = ' ';
+for (j = i; k < n; j++)
+{
+dest[j] = src[k];
+k++;
+if (src[k] == '\0')
+{
+break;
+}
+}
+}
+return (dest);
+}
