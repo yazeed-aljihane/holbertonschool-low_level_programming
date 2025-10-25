de8104187962742519131d52e6a8a92dbfb8396b
Your Name <you@example.com>
2025-10-25 08:01:01 +0000

    str

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+
+/**
+ * *_strcat - append str2 to str1
+ * @dest: str1
+ * @src:  str2
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+int k = 0;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+
+
+dest[i] = ' ';
+for (j = i; src[k] != '\0'; j++)
+{
+dest[j] = src[k];
+k++;
+}
+return (dest);
+}
--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -8,24 +8,15 @@
  */
 int main(void)
 {
-int nb;
+char s1[98] = "Hello ";
+char s2[] = "World!\n";
+char *ptr;
 
-nb = _atoi("98");
-printf("%d\n", nb);
-nb = _atoi("-402");
-printf("%d\n", nb);
-nb = _atoi("          ------++++++-----+++++--98");
-printf("%d\n", nb);
-nb = _atoi("214748364");
-printf("%d\n", nb);
-nb = _atoi("0");
-printf("%d\n", nb);
-nb = _atoi("Suite 402");
-printf("%d\n", nb);
-nb = _atoi("         +      +    -    -98 Battery Street; "
-"San Francisco, CA 94111 - USA             ");
-printf("%d\n", nb);
-nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-printf("%d\n", nb);
+printf("%s\n", s1);
+printf("%s", s2);
+ptr = _strcat(s1, s2);
+printf("%s", s1);
+printf("%s", s2);
+printf("%s", ptr);
 return (0);
 }
