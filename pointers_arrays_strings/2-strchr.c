90efd55cd97c1e411bac5d1e2b8e0ec7b640bfe1
Your Name <you@example.com>
2025-10-27 08:00:12 +0000

    ll

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -28,32 +28,6 @@ return (NULL);
 
 
 
-/*
- *
- *char *_strchr(char *s, char c)
-{
-int i, j;
-char t[98];
-char *p = t;
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] == c)
-{
-for (j = 0; s[j] != '\0'; j++)
-{
-p[j] = s[i];
-i++;
-}
-break;
-}
-}
-
-if (t == NULL)
-return (NULL);
-else
-return (p);
-}
- * */
 
 
 
