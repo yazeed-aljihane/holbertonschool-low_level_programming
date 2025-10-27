0e956a5072153c5e84051ab6b1d93ba1d80190c6
Your Name <you@example.com>
2025-10-27 07:59:18 +0000

    ll

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,6 +10,9 @@
 char *_strchr(char *s, char c)
 {
 int i;
+
+if (c != '\0')
+{
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] == c)
@@ -17,7 +20,9 @@ if (s[i] == c)
 	return (s + i);
 }
 }
-return (0);
+}
+
+return (NULL);
 }
 
 
