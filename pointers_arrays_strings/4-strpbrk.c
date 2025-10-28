4e1e7abdf07a6f957b2d70b0236e2891d6acc5e4
Your Name <you@example.com>
2025-10-28 07:49:09 +0000

    wow

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+
+/**
+ * *_strpbrk -  function that searches a string for any of a set of bytes.
+ * @s: string
+ * @accept: string
+ * Return: pointer to the latter
+ */
+
+
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+	if (s[i] == accept[j])
+	{
+	break;
+	}
+	}
+
+	if (s[i] == accept[j])
+	{
+	break;
+	}
+
+}
+return (s + i);
+}
+
+
+
+
+
+
+
