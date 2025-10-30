404c79638639d38b56c30b3c6bb80b50a23a9ae8
Your Name <you@example.com>
2025-10-30 06:12:45 +0000

    main

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+/**
+ * main - the main porgram
+ * @argc: the argument count
+ * @argv: array of arugment
+ * Return: 0 if succsus error otherwise
+ */
+int main(int argc, char **argv)
+{
+
+printf("%s\n", argv[argc - 1]);
+
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
