05d27c29bfed89170857886bbf344010b5a1486c
Your Name <you@example.com>
2025-10-30 07:05:22 +0000

    dd

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - the main porgram
+ * @argc: the argument count
+ * @argv: array of arugment
+ * Return: 0 if succsus error otherwise
+ */
+int main(int argc, char **argv)
+{
+(void)argv;
+
+printf("%d\n", argc - 1);
+
+return (0);
+}
+
