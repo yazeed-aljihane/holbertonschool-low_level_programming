d6c7c0ce3a806bc353067da180ccb940885b65d8
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-03 14:23:32 +0300

    maloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars and initializes it with a char.
+ * @size: the size of the array
+ * @c: the character used to initialize the array
+ * Return: pointer to the array, or NULL if size is 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *arr = malloc(size);
+
+for (i = 0; i < size; i++)
+{
+arr[i] = c;
+}
+
+return (arr);
+}
---
+++ malloc_free/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+if (write(1, &c, 1) == 1) 
+return 0;
+else
+return -1;
+}
