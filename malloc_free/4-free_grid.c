e8615b3042c798795c2a5c78c2a0b846be6a9575
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-04 10:09:45 +0300

    free array

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -5,11 +5,22 @@
 
 
 
-
+/**
+ * free_grid - Frees a 2D grid previously allocated by alloc_grid
+ * @grid: Pointer to the 2D array to be freed
+ * @height: Number of rows in the grid
+ *
+ * Description:
+ * This function releases all memory allocated for a two-dimensional
+ * grid of integers. It first frees each row of the grid, then frees
+ * the array of pointers itself to avoid memory leaks.
+ *
+ * Return: Nothing (void)
+ */
 void free_grid(int **grid, int height)
 {
 int i;
-for(i = 0; i < height; i++)
+for (i = 0; i < height; i++)
 {
 free(grid[i]);
 }
