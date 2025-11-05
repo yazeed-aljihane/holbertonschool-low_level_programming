8594b910e88c09c0e3ef704b52ec182d20aaae9f
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-05 16:43:46 +0300

    zez

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *arr;
-
+int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
 
@@ -26,7 +26,10 @@ arr = malloc(nmemb * size);
 if (arr == NULL)
 return (NULL);
 
-
+for (i = 0; i < nmemb; i++)
+{
+arr[i] = 0;
+}
 
 
 return (arr);
