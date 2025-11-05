f0493138b7226fd9a8676c4d3df7f99301bd6f4b
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-05 13:54:24 +0300

    zez

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,10 +5,23 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i,j;
+unsigned int i, j;
 char *arr;
+if (s1 == NULL)
+s1 = "";
 
-for (i = 0; s1[i] != '\0'; i++);
+if (s2 == NULL)
+s2 = "";
+
+for (i = 0; s2[i] != '\0'; i++)
+;
+if (n >= i)
+{
+n = i; 
+}
+
+for (i = 0; s1[i] != '\0'; i++)
+;
 
 
 arr = malloc(i + n + 1);
@@ -20,7 +33,7 @@ for (j = 0; j < i; j++)
 arr[j] = s1[j];
 }
 
-for (j = 0; j <= n; j++)
+for (j = 0; j < n; j++)
 {
 arr[i] = s2[j];
 i++;
