27da5f839cea92776b3290cd33a949b1b4572f38
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-05 13:56:37 +0300

    finish

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,6 +3,18 @@
 
 
 
+/**
+ * string_nconcat - concatenates two strings, using at most n bytes from s2
+ * @s1: first string
+ * @s2: second string
+ * @n: maximum number of bytes from s2 to concatenate
+ *
+ * Description: Allocates memory and returns a pointer to a new string that
+ * contains s1 followed by the first n bytes of s2. If s1 or s2 is NULL,
+ * they are treated as empty strings. If malloc fails, function returns NULL.
+ *
+ * Return:pointer to the newly allocated concatenated string,or NULL on failure
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i, j;
@@ -17,7 +29,7 @@ for (i = 0; s2[i] != '\0'; i++)
 ;
 if (n >= i)
 {
-n = i; 
+n = i;
 }
 
 for (i = 0; s1[i] != '\0'; i++)
