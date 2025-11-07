66c16e2365d47ea5ac57b7514426af20b6b906d4
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-07 14:08:11 +0300

    zez

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,10 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
-
-
 /**
  * new_dog - Creates a new dog structure
  * @name: Pointer to the dog's name
@@ -23,41 +19,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *newdog;
 char *name_cp;
 char *owner_cp;
-int i,j,z;
-
-
-for(j = 0; name[j] != '\0';j++)
+int i, j, z;
+for (j = 0; name[j] != '\0'; j++)
 ;
-
-for(z = 0; owner[z] != '\0';z++)
+for (z = 0; owner[z] != '\0'; z++)
 ;
 name_cp = malloc(j + 1);
 if (name_cp == NULL)
 return (NULL);
 for (i = 0; name[i] != '\0'; i++)
-{
 name_cp[i] = name[i];
-}
 name_cp[i] = '\0';
-
 owner_cp = malloc(z + 1);
 if (owner_cp == NULL)
-return (NULL);
-for (i = 0; owner[i] != '\0'; i++)
 {
-owner_cp[i] = owner[i];
+free(name_cp);
+return (NULL);
 }
+for (i = 0; owner[i] != '\0'; i++)
+owner_cp[i] = owner[i];
 owner_cp[i] = '\0';
-
 newdog = malloc(sizeof(dog_t));
 if (newdog == NULL)
 {
-free(newdog);
+free(owner_cp);
+free(name_cp);
 return (NULL);
 }
 newdog->name = name_cp;
 newdog->age = age;
 newdog->owner = owner_cp;
-
 return (newdog);
 }
