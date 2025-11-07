0cce032a1c8ef7aa228d2e6f60b07c0a841784ce
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-07 13:04:47 +0300

    zez

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -20,13 +20,14 @@ return;
 
 if (d->name == NULL)
 printf("name: (nil)\n");
+else
+printf("Name: %s\n", d->name);
+
+printf("Age: %.6f\n", d->age);
 
 if (d->owner == NULL)
 printf("owner: (nil)\n");
-
-printf("Name: %s\n", d->name);
-printf("Age: %.6f\n", d->age);
+else
 printf("Owner: %s\n", d->owner);
-
 
 }
