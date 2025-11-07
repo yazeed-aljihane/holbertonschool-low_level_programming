ed7534ec5325ac0a90943446e407fbe36ac4b3f7
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-07 14:20:24 +0300

    zez

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,13 +1,22 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 
 
 
-
+/**
+ * free_dog - Frees memory allocated for a dog_t structure
+ * @d: Pointer to the dog_t structure to be freed
+ *
+ * Description: Safely releases all memory associated with a dog_t structure,
+ * including the dynamically allocated name and owner fields. If @d is NULL,
+ * the function does nothing.
+ */
 void free_dog(dog_t *d)
 {
+if (d == NULL)
+return;
+
 free(d->name);
 free(d->owner);
 free(d);
