fbec68f57597edc665b0eda4f57d7d470ac447a0
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-12 08:19:44 +0300

    zez

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -22,18 +22,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int (*fun)(int) = cmp;
 int i;
-int total = 0;
 if (size <= 0)
 return (-1);
 if (array == NULL)
+return (-1);
+if (cmp == NULL)
 return (-1);
 
 for (i = 0; i < size; i++)
 {
 if (fun(array[i]) > 0)
-total += fun(array[i]);
+return (i);
 }
-if (total == 0)
 return (-1);
-return (total);
 }
