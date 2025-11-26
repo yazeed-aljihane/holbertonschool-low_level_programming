7f91234f415d86b6253ffe95d781e3680703be62
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-26 10:14:38 +0300

    add: add new function that adds new node in the begining of list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -24,6 +24,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newnode->n = n;
 	newnode->prev = NULL;
 	newnode->next = *head;
+	if (*head != NULL)
+		(*head)->prev = newnode;
+
 	*head = newnode;
 
 	return (newnode);
