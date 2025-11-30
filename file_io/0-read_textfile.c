bba4aa1fe670a54d54534bc40beab79aea3a04ef
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-11-30 12:00:36 +0300

    add: new function that read files

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -21,18 +21,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	if (filename == NULL)
-		return (-1);
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (-1);
+		return (0);
 
 	count = read(fd, buffer, letters);
 	if (count == -1)
-		return (-1);
+		return (0);
 
 	finalCount = write(1, buffer, count);
 	if (finalCount != count)
-		return (-1);
+		return (0);
 	close(fd);
 	free(buffer);
 	return (finalCount);
