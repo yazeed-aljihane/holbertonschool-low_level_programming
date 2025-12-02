1d60dbca3d26bfdd2f96a3e274a7981bf3d87470
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-12-02 10:59:20 +0300

    add: new function that create new file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+
+
+
+/**
+* create_file -  function that creates a file.
+* @filename: the name of file to create
+* @text_content: the contint of new file
+* Return: return 1 if success or -1 if fails
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t writeFile, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	for (i = 0; text_content[i]; i++)
+		;
+
+	writeFile = write(fd, text_content, i);
+	if (writeFile == -1 || writeFile != i)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
