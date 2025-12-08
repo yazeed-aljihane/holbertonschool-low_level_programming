bf9e9f60da11a07384ef50452899f5901941bbd5
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-12-08 13:59:33 +0300

    add: add  function create a hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+
+
+
+/**
+* hash_table_create - function that creates a hash table.
+* @size: the size of array
+* Return: pointer to hash table or NULL if fails
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *tableptr;
+	hash_node_t **array;
+
+
+
+	tableptr = malloc(sizeof(hash_table_t));
+	if (tableptr == NULL)
+		return (NULL);
+
+	array = calloc(size, sizeof(*hash_node_t) * size);
+	if (array == NULL)
+	return (NULL);
+
+	tableptr->size = size;
+	tableptr->array = array;
+
+	return (tableptr);
+}
