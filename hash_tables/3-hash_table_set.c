9143aa1bf32a8ae1c580e778fb495befa9a5ee53
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-12-08 19:01:08 +0300

    add: add  function add new node

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -30,6 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (value == NULL)
 		value = "";
+	
 	index = hash_djb2((const unsigned char *)key) % ht->size;
 	tmp = ht->array[index];
 	while(tmp)
